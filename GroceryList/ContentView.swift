fcffd4e741bacaa52699aef21387595e206bc13b
shouta0715 <kshouta0715@gmail.com>
2025-01-04 21:58:19 +0900

    add: input area

--- GroceryList/ContentView.swift
+++ GroceryList/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
   @Environment(\.modelContext) private var modelContext
   @Query private var items: [Item]
   
+  @State private var item: String = ""
+  
+  @FocusState private var isFocused: Bool
+  
   func addEssentialFoods() {
     modelContext.insert(Item(title: "Apples", isCompleted: false))
     modelContext.insert(Item(title: "Bananas", isCompleted: false))
@@ -76,6 +80,34 @@ struct ContentView: View {
               )
             )
           }
+        }
+        .safeAreaInset(edge: .bottom) {
+          VStack(spacing: 12) {
+            TextField("", text: $item)
+              .textFieldStyle(.plain)
+              .padding(12)
+              .background(.tertiary)
+              .cornerRadius(12)
+              .font(.title.weight(.light))
+              .focused($isFocused)
+            
+            Button {
+              guard !item.isEmpty else { return }
+              let newItem = Item(title: self.item, isCompleted: false)
+              modelContext.insert(newItem)
+              item = ""
+              isFocused = false
+            } label: {
+              Text("Save")
+                .font(.title2.weight(.medium))
+                .frame(maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle)
+            .controlSize(.extraLarge)
+          }
+          .padding()
+          .background(.bar)
         }
       }
     }
