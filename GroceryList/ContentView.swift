eda4b98b149622a5c61838fb6399618fe402d044
shouta0715 <kshouta0715@gmail.com>
2025-01-04 20:51:17 +0900

    Initial Commit

---
+++ GroceryList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryList
+//
+//  Created by k21047kk on 2025/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by k21047kk on 2025/01/04.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
